660ae657276b8689a72e33d88a79d4029135e666
Haemanth S P <haemanthsp@gmail.com>
2019-11-07 16:35:13 +0100

    Exercise 1

---
+++ Exercise1/exercisesheet01/src/main/java/de/cogmod/anns/exercisesheet01/MLPXOR.java
@@ -0,0 +1,59 @@
+package de.cogmod.anns.exercisesheet01;
+
+import java.util.Random;
+
+import de.cogmod.anns.exercisesheet01.MultiLayerPerceptron;
+import de.cogmod.anns.exercisesheet01.misc.BasicLearningListener;
+
+/**
+ * @author Sebastian Otte
+ */
+public class MLPXOR {
+    
+    public static void main(String[] args) {
+        //
+        final double[][] input = {
+            //{0, 0}, {0, 1}, {1, 0}, {1, 1}
+            {0}
+        };
+        final double[][] target = {
+            {1}
+        };
+        //
+        final Random rnd = new Random(100L);
+        //
+        // set up network. biases are used by default, but
+        // be deactivated using net.setBias(layer, false),
+        // where layer gives the layer index (1 = the first hidden layer).
+        //
+        final MultiLayerPerceptron net = new MultiLayerPerceptron(1, 100,  1);
+
+        //net.setBias(0,false);
+        net.setBias(1,false);
+        //net.setBias(2,false);
+        //net.setBias(3,false);
+        //net.setBias(4,false);
+        //
+        // perform training.
+        //
+        final int epochs = 10000;         // don't change this value!
+        final double learningrate = 0.01;
+        final double momentumrate = 0.95;
+        //
+        // generate initial weights.
+        //
+        net.initializeWeights(rnd, 0.1);
+        //
+        net.trainStochastic(
+            rnd, 
+            input,
+            target,
+            epochs,
+            learningrate,
+            momentumrate,
+            new BasicLearningListener()
+        );
+        //
+    }
+
+}
